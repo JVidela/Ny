4f1b53634cc1d1a335e07cd425aa89ee6d37fb1e
JVidela <videlariveronicolas@gmail.com>
2015-12-30 14:59:17 -0300

    Correccion de configuracion de los protectos

--- DataAccess/Modelo.Designer.cs
+++ DataAccess/Modelo.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'c:\Users\JVidela\documents\visual studio 2013\Projects\SGE\DataAccess\SGE.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\JVidela\documents\visual studio 2013\Projects\SGE\DataAccess\Modelo.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
